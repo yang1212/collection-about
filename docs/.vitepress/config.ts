e0d4af06de7adfa0481b6f723b09174cf8659f10
杨伏 <1204914685@qq.com>
2025-05-16 12:46:34 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+    title: '个人图书馆',
+    themeConfig: {
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '语言', link: '/language/' },
+            { text: '框架', link: '/framework/Vue2.md' },
+        ]
+    }
+})
